d352238bfc9b17f8340848a48076815da1f474c4
Kevin Loughead <kvnloughead@gmail.com>
2020-01-04 14:51:00 -0500

    Add item message added

--- completed_projects/simple-todo/todo.js
+++ completed_projects/simple-todo/todo.js
@@ -1,18 +1,18 @@
 var todos = ["Buy New Turtle", "Feed New Turtle"];
 
-var input = prompt("What would you like to do?");
-
 while(input !== "quit"){
-    //handle input
+    var input = prompt("What would you like to do?");
+    
     if(input === "list"){
+        //list items
         console.log(todos);
-    } else if(input === "new") {
-        //add to todo list
+    } 
+    else if(input === "new") {
+        //add item to list
         var newTodo = prompt("Enter new todo:");
         todos.push(newTodo);
+        console.log(newTodo + " added to list");
     }
-    //request new input
-    input = prompt("What would you like to do?");
 }
 
 //exit app
