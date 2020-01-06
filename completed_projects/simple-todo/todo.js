68187cbd3a46c48f8cd6c2450a8b685df5ebb3ad
Kevin Loughead <kvnloughead@gmail.com>
2020-01-05 22:17:22 -0500

    Refactored todo list into functions

--- completed_projects/simple-todo/todo.js
+++ completed_projects/simple-todo/todo.js
@@ -4,36 +4,41 @@ while(input !== "quit"){
     var input = prompt("What would you like to do?");
     
     if(input === "list"){
-        //list items
-        console.log("Here is your todo list:")
-        todos.forEach(function(item, i){
-            console.log(i + ": " + item)
-        });
-        console.log("======================");
-    } 
-    else if(input === "new") {
-        //add item to list
-        var newItem = prompt("Enter new todo:");
-        todos.push(newItem);
-        console.log(newItem + " added to list");
-    }
-    else if(input === "delete"){
-        var doneItem = prompt(`Which item would you like to delete?\n \
-        You may enter the entry itself, or its index.`)
-        if (Number.isInteger(Number(doneItem)) && Number(doneItem) < todos.length){
-            console.log("Todo #" + doneItem + ": '" + todos[doneItem] + "' removed from list")
-            todos.splice(doneItem, 1)
-        } 
-        else {
-        //else, doneItem is an entry
-            console.log("Todo #" + todos.indexOf(doneItem) + ": '" + doneItem + "' removed from list")
-            var todos = todos.filter(function(item){
-                return item !== doneItem
-            });
-            
-        }
+        listTodos()
+    } else if(input === "add") {
+        addTodo()
+    } else if(input === "delete"){
+        var todos = deleteTodo(todos)
     }
 }
 
+function listTodos(){
+    console.log("Here is your todo list:")
+    console.log("----------------------");
+    todos.forEach(function(item, i){
+        console.log(i + ": " + item)
+    });
+    console.log("======================");
+}
+function addTodo(){
+    var newItem = prompt("Enter todo to add to list:");
+    todos.push(newItem);
+    console.log(newItem + " added to list");
+}
+function deleteTodo(todos){
+    var doneItem = prompt(`foo Which item would you like to delete?\n \
+    You may enter the entry itself, or its index.`.replace(/  +/g, '')) //Dedents by removing all double spaces (and apparently tabs), from the resulting string. 
+    if (Number.isInteger(Number(doneItem)) && Number(doneItem) < todos.length){
+        console.log("Todo #" + doneItem + ": '" + todos[doneItem] + "' removed from list")
+        todos.splice(doneItem, 1)
+    } 
+    else {
+        console.log("Hi Todo #" + todos.indexOf(doneItem) + ": '" + doneItem + "' removed from list")
+        var todos = todos.filter(function(item){
+            return item !== doneItem
+        });
+    return todos
+    }
+}
 //exit app
 console.log("Goodbye");
